f8a426b6fdf9d26034690d717e4aa192977c301a
Matthew Kao <mkao6187@stu.d214.org>
2023-04-04 14:40:31 -0500

    added 2 textfields and two variables

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var int = 0
+    @State var num = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField ("", value: $int, format: .number)
+            TextField ("", value: $num, format: .number)
         }
     }
 }
