1948979e314291d65fb5fcf0101c0f9a7fceefb2
Matthew Kao <mkao6187@stu.d214.org>
2023-04-04 14:51:28 -0500

    added 2 textfields and 2 var

--- ContentView.swift
+++ ContentView.swift
@@ -1,9 +1,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var int = 0
+    @State var num = 0
     @State var Answer = 0
     var body: some View {
         VStack {
+            TextField("", value: $int, format: .number)
+            TextField("", value: $num, format: .number)
             Button("Multiply") {
                 <#code#>
             }
